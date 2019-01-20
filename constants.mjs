8946fe7b98c8e7c16ecc479b2e352701dfbf4c78
Erez Nagar <erez.nagar@gmail.com>
2019-01-19 20:25:39 -0500

    Updating environment variables

--- constants.mjs
+++ constants.mjs
@@ -1,16 +1,13 @@
-import auth from "./auth.mjs";
-
 const CITY_ID = 4954380;
-const WEATHER_API_APP_ID = '299923ca11830593e9a967d0baebe671';
 
 export default {
-    NEST_AUTH_TOKEN: process.env.NODE_ENV === 'production'? process.env.NEST_AUTH_TOKEN : auth.NEST_AUTH_TOKEN,
-    FIREBASE_API_KEY: process.env.NODE_ENV === 'production'? process.env.FIREBASE_API_KEY : auth.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: 'nest-cfc9f.firebaseapp.com',
-    FIREBASE_DB_URL: 'https://nest-cfc9f.firebaseio.com',
+    NEST_AUTH_TOKEN: process.env.NEST_AUTH_TOKEN,
     NEST_API_URL: 'https://developer-api.nest.com',
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DB_URL: process.env.FIREBASE_DB_URL,
     DEVICE_NAME: '743iDJfdnbMJlB4mVsa8ooyk___AL8Jo',
-    WEATHER_API_URL: `http://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&APPID=${WEATHER_API_APP_ID}`,
+    WEATHER_API_URL: `http://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&APPID=${process.env.WEATHER_API_APP_ID}`,
     SAMPLE_INTERVAL: 60000, // 1 minute;
     THERMOSTAT_OFF: "off",
 }
